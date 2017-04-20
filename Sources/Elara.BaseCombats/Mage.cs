9877ccb64fe5fc736ab541f3e1af2fdc87988fbc
Vanguard08 <vanguard1291@yahoo.com>
2017-04-20 08:23:39 -0500

    Added Base Frost Spells and Auras

--- Sources/Elara.BaseCombats/Mage.cs
+++ Sources/Elara.BaseCombats/Mage.cs
@@ -23,54 +23,65 @@ namespace Elara.BaseCombats
             public bool UseBlizzard = false;
         }
 
-        private SpellInfo RuneOfPower;
-        private SpellInfo RayOfFrost;
-        private SpellInfo Flurry;
-        private SpellInfo IceLance;
-        private SpellInfo FrozenOrb;
-        private SpellInfo FrostBolt;
-        private SpellInfo FrostBomb;
-        private SpellInfo GlacialSpike;
-        private SpellInfo Blizzard;
+		private SpellInfo RuneOfPower;
+		private SpellInfo RayOfFrost;
+		private SpellInfo Flurry;
+		private SpellInfo IceLance;
+		private SpellInfo FrozenOrb;
+		private SpellInfo FrostBolt;
+		private SpellInfo FrostBomb;
+		private SpellInfo GlacialSpike;
+		private SpellInfo Blizzard;
+		private SpellInfo Ebonbolt;
+		private SpellInfo IcyVeins;
+		private SpellInfo MirrorImage;
+		private SpellInfo WaterJet;
 
-        private const int AURA_FINGERS_OF_FROST         = 44544;
-        private const int AURA_FROSTBOMB                = 112948;
-        private const int AURA_BRAIN_FREEZE             = 190446;
-        private const int AURA_RUNE_OF_POWER            = 116014;
-        private const int AURA_ICICLES                  = 205473;
-        private const int AURA_INCANTERS_FLOW           = 116267; 
 
-        private UI.UserControlMage m_Interface;
-        public MageSettings CurrentSetting { get; private set; } = new MageSettings();
+		private const int AURA_FINGERS_OF_FROST = 44544;
+		private const int AURA_FROSTBOMB = 112948;
+		private const int AURA_BRAIN_FREEZE = 190446;
+		private const int AURA_RUNE_OF_POWER = 116014;
+		private const int AURA_ICICLES = 205473;
+		private const int AURA_INCANTERS_FLOW = 116267;
 
-        public Mage(Elara p_Elara)
-            : base(p_Elara)
-        {
-        }
-        
-        public override string Name => "Mage";
+		private UI.UserControlMage m_Interface;
+		public MageSettings CurrentSetting { get; private set; } = new MageSettings();
 
-        public override string Author => "Elara";
+		public Mage(Elara p_Elara)
+			: base(p_Elara)
+		{
+		}
 
-        public override UserControl Interface => m_Interface;
+		public override string Name => "Mage";
 
-        public override void OnLoad()
-        {
-            Elara.SettingsManager.OnLoadSettings += SettingsManager_OnLoadSettings;
-            Elara.SettingsManager.OnSaveSettings += SettingsManager_OnSaveSettings;
+		public override string Author => "Elara";
 
-            m_Interface     = new UI.UserControlMage(this);
-            LoadSettings(Elara.SettingsManager);
+		public override UserControl Interface => m_Interface;
 
-            RuneOfPower     = new SpellInfo(Game, 116011);
-            RayOfFrost      = new SpellInfo(Game, 205021);
-            IceLance        = new SpellInfo(Game, 30455);
-            Flurry          = new SpellInfo(Game, 44614);
-            FrozenOrb       = new SpellInfo(Game, 84714);
-            FrostBolt       = new SpellInfo(Game, 116);
-            FrostBomb       = new SpellInfo(Game, 112948);
-            GlacialSpike    = new SpellInfo(Game, 199786);
-            Blizzard        = new SpellInfo(Game, 190356);
+		public override void OnLoad()
+		{
+			Elara.SettingsManager.OnLoadSettings += SettingsManager_OnLoadSettings;
+			Elara.SettingsManager.OnSaveSettings += SettingsManager_OnSaveSettings;
+
+			m_Interface = new UI.UserControlMage(this);
+			LoadSettings(Elara.SettingsManager);
+
+			RuneOfPower = new SpellInfo(Game, 116011);
+			RayOfFrost = new SpellInfo(Game, 205021);
+			IceLance = new SpellInfo(Game, 30455);
+			Flurry = new SpellInfo(Game, 44614);
+			FrozenOrb = new SpellInfo(Game, 84714);
+			FrostBolt = new SpellInfo(Game, 116);
+			FrostBomb = new SpellInfo(Game, 112948);
+			GlacialSpike = new SpellInfo(Game, 199786);
+			Blizzard = new SpellInfo(Game, 190356);
+			Ebonbolt = new SpellInfo(Game, 214634);
+			IcyVeins = new SpellInfo(Game, 12472);
+			MirrorImage = new SpellInfo(Game, 55342);
+			WaterJet = new SpellInfo(Game, 135029);
+
+
         }
 
         public override void OnUnload()
