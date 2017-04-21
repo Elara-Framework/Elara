58caed32fada6dea0bca246cdd81e86addd50d9d
Vanguard08 <vanguard1291@yahoo.com>
2017-04-21 09:33:22 -0500

    UI Added UseCDs & Added Spells

--- Sources/Elara.BaseCombats/Mage.cs
+++ Sources/Elara.BaseCombats/Mage.cs
@@ -21,72 +21,69 @@ namespace Elara.BaseCombats
         {
             public bool UseFrozenOrb = true;
             public bool UseBlizzard = false;
-			public bool UseCoolDowns = false;
+
+            public bool UseCDs = true;
         }
 
-		private SpellInfo RuneOfPower;
-		private SpellInfo RayOfFrost;
-		private SpellInfo Flurry;
-		private SpellInfo IceLance;
-		private SpellInfo FrozenOrb;
-		private SpellInfo FrostBolt;
-		private SpellInfo FrostBomb;
-		private SpellInfo GlacialSpike;
-		private SpellInfo Blizzard;
-		private SpellInfo Ebonbolt;
-		private SpellInfo IcyVeins;
-		private SpellInfo MirrorImage;
-		private SpellInfo WaterJet;
-		private SpellInfo Blink;
-		private SpellInfo Shimmer;
+        private SpellInfo RuneOfPower;
+        private SpellInfo RayOfFrost;
+        private SpellInfo Flurry;
+        private SpellInfo IceLance;
+        private SpellInfo FrozenOrb;
+        private SpellInfo FrostBolt;
+        private SpellInfo FrostBomb;
+        private SpellInfo GlacialSpike;
+        private SpellInfo Blizzard;
+        private SpellInfo Ebonbolt;
+        private SpellInfo IcyVeins;
+        private SpellInfo MirrorImage;
+        private SpellInfo WaterJet;
+        private SpellInfo Blink;
+        private SpellInfo Shimmer;
 
+        private const int AURA_FINGERS_OF_FROST         = 44544;
+        private const int AURA_FROSTBOMB                = 112948;
+        private const int AURA_BRAIN_FREEZE             = 190446;
+        private const int AURA_RUNE_OF_POWER            = 116014;
+        private const int AURA_ICICLES                  = 205473;
 
-		private const int AURA_FINGERS_OF_FROST = 44544;
-		private const int AURA_FROSTBOMB = 112948;
-		private const int AURA_BRAIN_FREEZE = 190446;
-		private const int AURA_RUNE_OF_POWER = 116014;
-		private const int AURA_ICICLES = 205473;
-		private const int AURA_INCANTERS_FLOW = 116267;
+        private UI.UserControlMage m_Interface;
+        public MageSettings CurrentSetting { get; private set; } = new MageSettings();
 
-		private UI.UserControlMage m_Interface;
-		public MageSettings CurrentSetting { get; private set; } = new MageSettings();
+        public Mage(Elara p_Elara)
+            : base(p_Elara)
+        {
+        }
+        
+        public override string Name => "Mage";
 
-		public Mage(Elara p_Elara)
-			: base(p_Elara)
-		{
-		}
+        public override string Author => "Elara";
 
-		public override string Name => "Mage";
+        public override UserControl Interface => m_Interface;
 
-		public override string Author => "Elara";
+        public override void OnLoad()
+        {
+            Elara.SettingsManager.OnLoadSettings += SettingsManager_OnLoadSettings;
+            Elara.SettingsManager.OnSaveSettings += SettingsManager_OnSaveSettings;
 
-		public override UserControl Interface => m_Interface;
+            m_Interface     = new UI.UserControlMage(this);
+            LoadSettings(Elara.SettingsManager);
 
-		public override void OnLoad()
-		{
-			Elara.SettingsManager.OnLoadSettings += SettingsManager_OnLoadSettings;
-			Elara.SettingsManager.OnSaveSettings += SettingsManager_OnSaveSettings;
-
-			m_Interface = new UI.UserControlMage(this);
-			LoadSettings(Elara.SettingsManager);
-
-			RuneOfPower = new SpellInfo(Game, 116011);
-			RayOfFrost = new SpellInfo(Game, 205021);
-			IceLance = new SpellInfo(Game, 30455);
-			Flurry = new SpellInfo(Game, 44614);
-			FrozenOrb = new SpellInfo(Game, 84714);
-			FrostBolt = new SpellInfo(Game, 116);
-			FrostBomb = new SpellInfo(Game, 112948);
-			GlacialSpike = new SpellInfo(Game, 199786);
-			Blizzard = new SpellInfo(Game, 190356);
-			Ebonbolt = new SpellInfo(Game, 214634);
-			IcyVeins = new SpellInfo(Game, 12472);
-			MirrorImage = new SpellInfo(Game, 55342);
-			WaterJet = new SpellInfo(Game, 135029);
-			Blink = new SpellInfo(Game, 1953);
-			Shimmer = new SpellInfo(Game, 212653);
-
-
+            RuneOfPower = new SpellInfo(Game, 116011);
+            RayOfFrost = new SpellInfo(Game, 205021);
+            IceLance = new SpellInfo(Game, 30455);
+            Flurry = new SpellInfo(Game, 44614);
+            FrozenOrb = new SpellInfo(Game, 84714);
+            FrostBolt = new SpellInfo(Game, 116);
+            FrostBomb = new SpellInfo(Game, 112948);
+            GlacialSpike = new SpellInfo(Game, 199786);
+            Blizzard = new SpellInfo(Game, 190356);
+            Ebonbolt = new SpellInfo(Game, 214634);
+            IcyVeins = new SpellInfo(Game, 12472);
+            MirrorImage = new SpellInfo(Game, 55342);
+            WaterJet = new SpellInfo(Game, 135029);
+            Blink = new SpellInfo(Game, 1953);
+            Shimmer = new SpellInfo(Game, 212653);
         }
 
         public override void OnUnload()
@@ -160,44 +157,47 @@ namespace Elara.BaseCombats
                 var l_HostilesAroundTarget = CombatUtils.GetAttackersAroundPosition(this.Game, l_Target.Position, p_MaxRange: 8.0f);
 
                 if (l_LocalPlayer.CastingInfo == null &&                                // Not casting
+                    l_LocalPlayer.IsMoving == false &&                                  // Not moving
                     RuneOfPower.ChargesAvailable > 0 &&                                 // Do we have a charge
                     l_SpellController.CanUseSpell(RuneOfPower, l_Target))               // Use SpellController generic conditions
                 {
                     l_SpellController.UseSpell(RuneOfPower);
                     return;
                 }
-		if (CurrentSetting.UseBlizzard &&                                       // Check user setting
-		  l_HostilesAroundTarget.Count >= 1 &&                                // Check adds around target
-		  l_TargetVisibleOnScreen &&                                          // Check that target is visible to click on
-		  l_LocalPlayer.IsFacingHeading(l_Target, 1.5f) &&                    // Check target facing
-		  l_LocalPlayer.CastingInfo == null &&                                // Not casting
-		  l_SpellController.CanUseSpell(Blizzard, l_Target))                  // Use SpellController generic conditions
-		{
-			l_SpellController.UseSpell(Blizzard, l_Target.Position);
-			return;
-		}
-                
 
-
-		if (CurrentSetting.UseFrozenOrb &&                                      // Check user setting
-                   l_LocalPlayer.IsFacingHeading(l_Target, 0.3f) &&                    // Check target facing
-                   l_LocalPlayer.CastingInfo == null &&                                // Not casting
-                   l_SpellController.CanUseSpell(FrozenOrb, l_Target))                 // Use SpellController generic conditions
+                if (l_LocalPlayer.CastingInfo == null &&                                // Not casting
+                  l_LocalPlayer.IsMoving == false &&                                  // Not moving
+                  CurrentSetting.UseCDs )
+                 
+                   
+                {
+                    l_SpellController.UseSpell(IcyVeins);
+                    return;
+                }
+                if (CurrentSetting.UseFrozenOrb &&                                      // Check user setting
+                    l_LocalPlayer.IsMoving == false &&                                  // Not moving
+                    l_LocalPlayer.IsFacingHeading(l_Target, 0.3f) &&                    // Check target facing
+                    l_LocalPlayer.CastingInfo == null &&                                // Not casting
+                    l_SpellController.CanUseSpell(FrozenOrb, l_Target))                 // Use SpellController generic conditions
                 {
                     l_SpellController.UseSpell(FrozenOrb);
                     return;
                 }
 
-
-		if (CurrentSetting.UseCoolDowns &&                                       // Check user setting
-					Shimmer.ChargesAvailable > 1) 
-		{
-					l_SpellController.UseSpell(Shimmer);
-			return;
-		}
-              
+                if (CurrentSetting.UseBlizzard &&                                       // Check user setting
+                    l_LocalPlayer.IsMoving == false &&                                  // Not moving
+                    l_HostilesAroundTarget.Count >= 2 &&                                // Check adds around target
+                    l_TargetVisibleOnScreen &&                                          // Check that target is visible to click on
+                    l_LocalPlayer.IsFacingHeading(l_Target, 1.5f) &&                    // Check target facing
+                    l_LocalPlayer.CastingInfo == null &&                                // Not casting
+                    l_SpellController.CanUseSpell(Blizzard, l_Target))                  // Use SpellController generic conditions
+                {
+                    l_SpellController.UseSpell(Blizzard, l_Target.Position);
+                    return;
+                }
 
                 if (l_LocalPlayer.CastingInfo == null &&                                // Not casting
+                    l_LocalPlayer.IsMoving == false &&                                  // Not moving
                     l_LocalPlayer.IsFacingHeading(l_Target, 1.5f) &&                    // Check target facing
                     l_LocalPlayer.GetAuraById(AURA_RUNE_OF_POWER) == null &&            // Check that we don't have Rune of Power buff
                     l_SpellController.CanUseSpell(RayOfFrost, l_Target))                // Use SpellController generic conditions
@@ -226,6 +226,7 @@ namespace Elara.BaseCombats
 
                 /* TODO : Check adds around target */
                 if (l_LocalPlayer.CastingInfo == null &&                                // Not casting
+                    l_LocalPlayer.IsMoving == false &&                                  // Not moving
                     l_LocalPlayer.IsFacingHeading(l_Target, 1.5f) &&                    // Check target facing
                     l_LocalPlayer.GetAuraById(AURA_FINGERS_OF_FROST)?.Stack >= 1 &&     // Check for proc
                     l_Target.GetAuraById(AURA_FROSTBOMB) == null &&                     // Check if buff is not already applied
@@ -236,6 +237,7 @@ namespace Elara.BaseCombats
                 }
 
                 if (l_LocalPlayer.CastingInfo == null &&                                // Not casting
+                    l_LocalPlayer.IsMoving == false &&                                  // Not moving
                     l_LocalPlayer.IsFacingHeading(l_Target, 1.5f) &&                    // Check target facing
                     l_LocalPlayer.GetAuraById(AURA_ICICLES)?.Stack >= 5 &&              // Check for proc
                     l_SpellController.CanUseSpell(GlacialSpike, l_Target))              // Use SpellController generic conditions
@@ -245,6 +247,7 @@ namespace Elara.BaseCombats
                 }
 
                 if (l_LocalPlayer.CastingInfo == null &&                                // Not casting
+                    l_LocalPlayer.IsMoving == false &&                                  // Not moving
                     l_LocalPlayer.IsFacingHeading(l_Target, 1.5f) &&                    // Check target facing
                     l_SpellController.CanUseSpell(FrostBolt, l_Target))                 // Use SpellController generic conditions
                 {
