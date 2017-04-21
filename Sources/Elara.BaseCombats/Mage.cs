c7d2c0615945c3272d4aa3d5e5765102efbc0450
Vanguard1291 <vanguard1291@yahoo.com>
2017-04-21 11:51:59 -0500

    Added back in Mage.cs info

--- Sources/Elara.BaseCombats/Mage.cs
+++ Sources/Elara.BaseCombats/Mage.cs
@@ -32,12 +32,18 @@ namespace Elara.BaseCombats
         private SpellInfo FrostBomb;
         private SpellInfo GlacialSpike;
         private SpellInfo Blizzard;
+        private SpellInfo Ebonbolt;
+        private SpellInfo IcyVeins;
+        private SpellInfo MirrorImage;
+        private SpellInfo WaterJet;
+        private SpellInfo Blink;
+        private SpellInfo Shimmer;
 
-        private const int AURA_FINGERS_OF_FROST         = 44544;
-        private const int AURA_FROSTBOMB                = 112948;
-        private const int AURA_BRAIN_FREEZE             = 190446;
-        private const int AURA_RUNE_OF_POWER            = 116014;
-        private const int AURA_ICICLES                  = 205473;
+        private const int AURA_FINGERS_OF_FROST = 44544;
+        private const int AURA_FROSTBOMB = 112948;
+        private const int AURA_BRAIN_FREEZE = 190446;
+        private const int AURA_RUNE_OF_POWER = 116014;
+        private const int AURA_ICICLES = 205473;
 
         private UI.UserControlMage m_Interface;
         public MageSettings CurrentSetting { get; private set; } = new MageSettings();
@@ -61,15 +67,21 @@ namespace Elara.BaseCombats
             m_Interface     = new UI.UserControlMage(this);
             LoadSettings(Elara.SettingsManager);
 
-            RuneOfPower     = new SpellInfo(Game, 116011);
-            RayOfFrost      = new SpellInfo(Game, 205021);
-            IceLance        = new SpellInfo(Game, 30455);
-            Flurry          = new SpellInfo(Game, 44614);
-            FrozenOrb       = new SpellInfo(Game, 84714);
-            FrostBolt       = new SpellInfo(Game, 116);
-            FrostBomb       = new SpellInfo(Game, 112948);
-            GlacialSpike    = new SpellInfo(Game, 199786);
-            Blizzard        = new SpellInfo(Game, 190356);
+            RuneOfPower = new SpellInfo(Game, 116011);
+            RayOfFrost = new SpellInfo(Game, 205021);
+            IceLance = new SpellInfo(Game, 30455);
+            Flurry = new SpellInfo(Game, 44614);
+            FrozenOrb = new SpellInfo(Game, 84714);
+            FrostBolt = new SpellInfo(Game, 116);
+            FrostBomb = new SpellInfo(Game, 112948);
+            GlacialSpike = new SpellInfo(Game, 199786);
+            Blizzard = new SpellInfo(Game, 190356);
+            Ebonbolt = new SpellInfo(Game, 214634);
+            IcyVeins = new SpellInfo(Game, 12472);
+            MirrorImage = new SpellInfo(Game, 55342);
+            WaterJet = new SpellInfo(Game, 135029);
+            Blink = new SpellInfo(Game, 1953);
+            Shimmer = new SpellInfo(Game, 212653);
         }
 
         public override void OnUnload()
